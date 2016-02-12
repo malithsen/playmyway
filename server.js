4337a8709a9b24b5ab5861bd179897fd221b9311
Hasanga Somaratne <hasanga.charaka@gmail.com>
2016-02-12 08:20:49 -0500

    Add functions to stop/pause and redirect to main page

--- server.js
+++ server.js
@@ -6,18 +6,8 @@ var express = require('express'),
      _ = require('lodash'),
     async = require('async'),
     Player = require('player'),
-    // lame = require('lame'),
     fs = require('fs');
-    // Speaker = require('speaker'),
-    // volume = require("pcm-volume");
-
-// var audioOptions = {
-//     channels: 2,
-//     bitDepth: 16,
-//     sampleRate: 44100,
-//     mode: lame.STEREO
-// };
-
+    
 var PATH = '/home/hasa93/Songs/';
 var player;
 
@@ -35,29 +25,6 @@ app.use(express.static(__dirname + '/public'));
 var mongocon = new MongoCon(config.mongo.uri);
 mongocon.init();
 
-
-// function playStream(input, options) {
-//   var decoder = lame.Decoder();
-//   options = options || {};
-//   var v = new volume();
-//   if (options.volume) {
-//     v.setVolume(options.volume);
-//   }
-//   var speaker = new Speaker(audioOptions);
-//   speaker.on('finish', function() {
-//     if (options.loop) {
-//       console.log('loop');
-//       start();
-//     }
-//   });
-//   function start() {
-//     console.dir(input);
-//     v.pipe(speaker);
-//     decoder.pipe(v);
-//     input.pipe(decoder);
-//   }
-//   start();
-// }
 
 app.get('/songs', function(req, res) {
   var cb = function(err, data) {
@@ -91,15 +58,21 @@ app.get('/play', function(req, res) {
 
       player = new Player(paths)
                .on('playing', function(song){
-                 console.log('Playing ' + song._name);                                 
+                 console.log(song);
+                 console.log('Playing ' + song._name); 
+                 mongocon.resetVotes(song.src); 
                 })
                .on('playend', function(song){
-                 console.log('Finished playlist!')
+                  console.log('Switching...');
                 })
                .on('error', function(err){
                  console.log(err);
                 })
                .play();    
+
+
+
+      res.redirect('/');
   });
   
 }); 
@@ -114,6 +87,17 @@ app.get('/next', function(req, res){
   }
 
   player.next();
+
+  res.redirect('/');
+
+});
+
+app.get('/stop', function(req, res){
+  console.log('Stopping current song...')
+  player.stop();
+
+  res.redirect('/');
+
 });
 
 app.get('/reload', function(req, res) {
@@ -137,7 +121,7 @@ app.get('/views/:v', function(req, res) {
   res.render(req.params.v);
 });
 
-app.get('*', function(req, res) {
+app.get('/', function(req, res) {
   console.log("calling layout");
   res.render('layout', {
     title: 'Quaker',
