94b8868cad197477e9904f82259b5c0daccabb49
Malith Senaweera <malithsenaweera@gmail.com>
2016-02-28 08:41:54 +0530

    Authentication. WIP

--- server.js
+++ server.js
@@ -3,14 +3,46 @@
 var express = require('express'),
     config = require('./config'),
     MongoCon = require('./MongoCon'),
-     _ = require('lodash'),
+    _ = require('lodash'),
     async = require('async'),
     Player = require('player'),
     fs = require('fs'),
     path = require('path'),
     http = require('http'),
-    Socket = require('socket.io');
+    Socket = require('socket.io'),
+    passport = require('passport'),
+    LocalStrategy = require('passport-local').Strategy;
 
+// Define the strategy to be used by PassportJS
+passport.use(new LocalStrategy(
+  function(username, password, done) {
+    console.log(username, password)
+    if (username === "admin" && password === "admin") // stupid example
+      return done(null, {name: "admin"});
+
+    return done(null, false);
+  }
+));
+
+// Serialized and deserialized methods when got from session
+passport.serializeUser(function(user, cb) {
+  cb(null, user);
+});
+
+passport.deserializeUser(function(id, cb) {
+  db.users.findById(id, function (err, user) {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+// Define a middleware function to be used for every secured routes
+var auth = function(req, res, next){
+  if (!req.isAuthenticated())
+    res.sendStatus(401);
+  else
+    next();
+};
 
 var PATH = ''; // When comitting keep this empty.
 var player, currSong;
@@ -39,6 +71,12 @@ io.on('connection', function(socket){
 app.set('views', __dirname + '/src/views');
 app.set('view engine', 'jade');
 app.locals.pretty = true;
+app.use(require('morgan')('combined'));
+app.use(require('cookie-parser')());
+app.use(require('body-parser').urlencoded({ extended: true }));
+app.use(require('express-session')({ secret: 'keyboard cat', resave: false, saveUninitialized: false }));
+app.use(passport.initialize()); // Add passport initialization
+app.use(passport.session());    // Add passport initialization
 
 
 app.use(express.static(__dirname + '/public'));
@@ -90,7 +128,7 @@ app.get('/upvote/:id', function(req, res) {
 
 });
 
-app.get('/play', function(req, res) {
+app.get('/play', auth, function(req, res) {
   console.log("playing");
 
   //stop an already plaing item
@@ -104,7 +142,7 @@ app.get('/play', function(req, res) {
   res.redirect('/');
 });
 
-app.get('/next', function(req, res){
+app.get('/next', auth, function(req, res){
   console.log('Switching to the next song...');
 
   if(typeof player === "undefined")
@@ -120,7 +158,7 @@ app.get('/next', function(req, res){
 
 });
 
-app.get('/stop', function(req, res){
+app.get('/stop', auth, auth, function(req, res){
   console.log('Stopping current song...')
   player.stop();
   playing = false;
@@ -130,13 +168,13 @@ app.get('/stop', function(req, res){
 
 });
 
-app.get('/pause', function(req, res){
+app.get('/pause', auth, function(req, res){
   player.pause();
   playing = !playing;
   res.redirect('/');
 });
 
-app.get('/reload', function(req, res) {
+app.get('/reload', auth, function(req, res) {
   console.log("Reload");
   fs.readdir(PATH, function(err, items) {
     res.json(items);
@@ -151,9 +189,26 @@ app.get('/reload', function(req, res) {
   });
 });
 
-app.get('/save', function(req, res){
+app.get('/save', auth, function(req, res){
   console.log("save song");
   mongocon.saveSong('test.mp3');
+});
+
+// route to test if the user is logged in or not
+app.get('/loggedin', function(req, res) {
+  res.send(req.isAuthenticated() ? req.user : '0');
+});
+
+// route to log in
+app.post('/login', passport.authenticate('local'), function(req, res) {
+  console.log(req);
+  res.send(req.user);
+});
+
+// route to log out
+app.post('/logout', function(req, res){
+  req.logOut();
+  res.send(200);
 });
 
 app.get('/views/:v', function(req, res) {
